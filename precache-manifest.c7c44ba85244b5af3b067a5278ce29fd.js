45937b412b98936373fb71f1f414387b2c42cd33
Hasitha Weerasinghe <43178708+hasithaweerasinghe@users.noreply.github.com>
2020-07-19 12:45:41 +0530

    Updates

---
+++ precache-manifest.c7c44ba85244b5af3b067a5278ce29fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "682ed37d6a8aef8e26bc9e446ba8e8c3",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "65f2957708308b10da34",
+    "url": "/react-gh-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c5c39051751acff5b36b",
+    "url": "/react-gh-pages/static/js/2.3ee159c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.3ee159c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65f2957708308b10da34",
+    "url": "/react-gh-pages/static/js/main.fea906d7.chunk.js"
+  },
+  {
+    "revision": "c910589a90c7db292e97",
+    "url": "/react-gh-pages/static/js/runtime-main.8d2d02b4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
